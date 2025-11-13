31d315fb9efff24e552498a0058552c457fc7c36
NotSanthosH <notsanthosh@NotSanthosHs-MacBook-Air.local>
2025-11-13 10:32:25 +0100

    fygyuf

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", 
+  base : process.env.VITE_BASE_PATH || "/portfolioV.2-veille",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
